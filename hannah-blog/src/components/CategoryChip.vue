f7c4e3c8b18dbc1dfbdcd069cb00459177de7d19
Hannah Willoughby <hannah@tcah.com>
2021-04-10 19:35:50 -0400

    Add category chip component

---
+++ hannah-blog/src/components/CategoryChip.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container fluid>
+    <v-chip
+      color="primary secondary--text"
+      small
+      style="margin-left: 5px"
+    >{{ category }}</v-chip>
+    <v-chip
+      v-for="subcategory in subcategories"
+      :key="subcategory"
+      color="accent"
+      small
+      style="margin-left: 5px"
+    >{{ subcategory }}</v-chip>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "CategoryChip",
+  props: [
+    "category",
+    "subcategories"
+  ]
+}
+</script>
