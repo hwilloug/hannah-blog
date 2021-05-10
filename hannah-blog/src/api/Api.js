b6e8b21b5a95ad924e9d5981f82d7c87854ef551
Hannah Willoughby <hannah@tcah.com>
2021-05-10 11:56:45 -0400

    Use local api for develoopment

--- hannah-blog/src/api/Api.js
+++ hannah-blog/src/api/Api.js
@@ -1,7 +1,14 @@
 import axios from 'axios'
 
+let url
+if (process.env.NODE_ENV == "development") {
+	url = "http://localhost:8080/api"
+} else {
+	url = "https://y014mdl7zl.execute-api.us-east-2.amazonaws.com/testing"
+}
+
 export default () => {
 	return axios.create({
-		baseURL: "https://y014mdl7zl.execute-api.us-east-2.amazonaws.com/testing"
+		baseURL: url
 	})
 }
