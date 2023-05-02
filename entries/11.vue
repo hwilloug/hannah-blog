e7e842874fd3629c2b3519bb532c739fcf694b22
Hannah Willoughby <hannah@tcah.com>
2023-05-02 16:47:03 -0400

    Add more to pig shelter article

--- entries/11.vue
+++ entries/11.vue
@@ -1,14 +1,13 @@
 <template>
 <v-card-text>
     <p>My dad and I build a pig shelter and pen!</p>
-    <img src="@/assets/pig_shelter_1.png" />
-    <p>Starting on the base</p>
     <img src="@/assets/pig_shelter_2.png" />
     <p>The finished base</p>
-    <img src="@/assets/pig_shelter_3.png" />
-    <p>Starting on the walls</p>
     <img src="@/assets/pig_shelter_4.png" />
-    <p>Most of the walls done</p>
+    <p>The frame in progress</p>
+    <img src="@/assets/pig_shelter_5.png" />
+    <p>The finished frame</p>
+    <p>At this point, we stained the entire frame so that it would be waterproof.</p>
 </v-card-text>
 </template>
 
