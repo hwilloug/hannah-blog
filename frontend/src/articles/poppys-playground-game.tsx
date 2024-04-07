8b551bcc8855dc7b9c9b917c7bf19283a4a7473c
Hannah Willoughby <hannah@tcah.com>
2024-04-07 09:55:22 -0400

    Add poppys playground article

---
+++ frontend/src/articles/poppys-playground-game.tsx
@@ -0,0 +1,70 @@
+import {
+  ArticleContentContainer,
+  FullSizeImage,
+  Section,
+} from "../components/StyledComponents";
+
+const PoppysPlaygroundGame: React.FC = () => {
+  return (
+    <ArticleContentContainer>
+      <Section>
+        <p>
+          Hello, friends! Long time no see 🌸 I've been jumping from hobby to
+          hobby lately, not able to finish anything before I start something
+          else. It's quite frustrating, but I am slowly making progress on a
+          bunch of things at once.
+        </p>
+        <p>
+          One of the things I've been working on is a game called Poppy's
+          Playground! It is written in{" "}
+          <a
+            href="https://en.wikipedia.org/wiki/Lua_(programming_language)"
+            target="_blank"
+          >
+            Lua
+          </a>
+          , and is pretty simple so far. Basically, you are my cat Poppy, and
+          you are trying to collect cookies without getting caught by the evil
+          Tiger.
+        </p>
+        <FullSizeImage
+          src={`${process.env.REACT_APP_IMAGES_BASE_URL}/poppys-playground-sample.gif`}
+          alt="Poppy's playground game, a game made with Lua"
+        />
+        <p>
+          I haven't deployed the game anywhere yet because I'll need to set up a
+          server to host the game, and those are expensive to run.
+        </p>
+        <p>
+          Future work includes:
+          <ul>
+            <li>
+              selecting a player at the beginning of the game (Poppy, Buddy, or
+              Ray). each player has different stats
+            </li>
+            <li>a high score board</li>
+            <li>adding tiger to chase the player</li>
+            <li>
+              allowing the player to jump on furniture to get away from Tiger
+            </li>
+            <li>adding catnip to give the player a boost</li>
+            <li>putting the game on a server to embed on this website</li>
+          </ul>
+        </p>
+        <p>
+          So there's still a lot of work to do before it's an actual, like, fun
+          game, but it's coming along, and I've learned a lot about Lua!{" "}
+          <a
+            href="https://github.com/hwilloug/poppys-playground"
+            target="_blank"
+          >
+            Click here
+          </a>{" "}
+          for the source code!
+        </p>
+      </Section>
+    </ArticleContentContainer>
+  );
+};
+
+export default PoppysPlaygroundGame;
