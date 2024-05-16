3fb78d859a83ce423135c173bb79f48654b3f4ee
Hannah Willoughby <hannah@tcah.com>
2024-05-16 14:53:13 -0400

    Schema modifications

--- backend/db/models/book_club_models.py
+++ backend/db/models/book_club_models.py
@@ -12,17 +12,21 @@ BookClubBase = declarative_base(metadata=metadata_schema)
 class Book(BookClubBase):
     __tablename__ = 'books'
 
-    book_id: Mapped[str] = mapped_column(primary_key=True)
+    book_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str]
     author: Mapped[str]
     link: Mapped[str]
     tags: Mapped[list[str]] = mapped_column(ARRAY(String))
     cover_image: Mapped[str]
+    start_date: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=True))
+    end_date: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=True))
+
 
 class Comment(BookClubBase):
     __tablename__ = "comments"
 
     comment_id: Mapped[str] = mapped_column(primary_key=True)
-    thread_id: Mapped[str] = mapped_column(ForeignKey("threads.thread_id"))
+    thread_id: Mapped[int] = mapped_column(ForeignKey("threads.thread_id"))
     created_at: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
     content: Mapped[str]
     username: Mapped[str]
@@ -31,17 +35,17 @@ class Comment(BookClubBase):
     parent_comment: Mapped["Comment"] = relationship("Comment", remote_side=[comment_id])
     reactions: Mapped[list["CommentReaction"]] = relationship("CommentReaction", back_populates="comment")
 
+
 class Thread(BookClubBase):
     __tablename__ = 'threads'
 
-    thread_id: Mapped[str] = mapped_column(primary_key=True)
-    book_id: Mapped[str] = mapped_column(ForeignKey("books.book_id"))
+    thread_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    book_id: Mapped[int] = mapped_column(ForeignKey("books.book_id"))
     title: Mapped[str]
-    description: Mapped[str]
-    start_date: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=True))
-    end_date: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=True))
+    description: Mapped[str] = mapped_column(nullable=True)
     last_comment_id: Mapped[str] = mapped_column(nullable=True)
     comments: Mapped[list[Comment]] = relationship("Comment", back_populates="thread")
+
 
 class ThreadParticipant(BookClubBase):
     __tablename__ = "thread_participants"
@@ -49,12 +53,14 @@ class ThreadParticipant(BookClubBase):
     thread_id: Mapped[str] = mapped_column(ForeignKey("threads.thread_id"), primary_key=True)
     username: Mapped[str]
 
+
 class CommentReadState(BookClubBase):
     __tablename__ = "comment_read_state"
 
     comment_id: Mapped[str] = mapped_column(ForeignKey("comments.comment_id"), primary_key=True)
     username: Mapped[str]
     read_date: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+
 
 class CommentReaction(BookClubBase):
     __tablename__ = "comment_reactions"
@@ -65,11 +71,12 @@ class CommentReaction(BookClubBase):
     username: Mapped[str]
     comment: Mapped["Comment"] = relationship("Comment", back_populates="reactions")
 
+
 class Review(BookClubBase):
     __tablename__ = "reviews"
 
     review_id: Mapped[str] = mapped_column(primary_key=True)
-    book_id: Mapped[str] = mapped_column(ForeignKey("books.book_id"))
+    book_id: Mapped[int] = mapped_column(ForeignKey("books.book_id"))
     comment_id: Mapped[str] = mapped_column(ForeignKey("comments.comment_id"))
     username: Mapped[str]
     rating: Mapped[float]
